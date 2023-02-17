88f6d99db618face69d2959c977b9fc3833e7c76
VitaminNeedsHelp <Luki.bayer@chello.at>
2023-02-17 11:50:17 +0100

    1st Semester End

--- Semester1/10.02.23/Average1.c
+++ Semester1/10.02.23/Average1.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    //durchschnittsberechnung zwischen x und y
 
+int avrg(int x, int y){
+    return (x+y)/2;
+}
+
+int main(void){
     int x, y;
     float avg;
 
@@ -12,7 +15,9 @@ int main(void){
     printf("Geben Sie y ein: ");
     scanf("%d", &y);
 
-    avg = (x + y) / 2.0;
+    avg = avrg(x, y);
 
     printf("Der Durchschnitt von %d und %d ist %.2f", x, y, avg);
+
 }
+
